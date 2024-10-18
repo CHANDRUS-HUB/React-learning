64ba0c02d98a96bddf97554b0416eb73ad0987ec
CHANDRUS-HUB <chandrumani72@gmail.com>
2024-10-19 05:17:26 +0530

    hooks practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    //usestate
+    //eg:1  
+    const [color, setColor] = useState('red')
+    function colorchange() {
+        setColor('blue')
+    }
+    const[count,setcount]=useState(0)
+    
+    //eg:2
+    const [car, setCar] = useState({ brand: 'bukati', cartype: 'gire', color: 'yellow' });
+    
+    function changecarproperties(){
+        setCar(()=>{
+            return{...car,brand: 'AAdi'}
+        })
+    }
+ //eg:3
+ const [names, setName] = useState(["ram","janani","romeo","juliate"]);
+ function addName(){
+    setName([...names,'raju','somu'])
+ }
+ function deleteName(){
+    setName(names.slice(0,names.length-2))
+ }
+    return (
+        <div>
+            {/* use state */}
+
+            {/* eg:1 */}
+            <h1>i like {color} color</h1>
+            <button onClick={colorchange}>changecolor</button>
+
+            {/* eg:2 */}
+             <div>
+                <h1>I like this car: {car.brand}</h1>
+                <h2>Type: {car.cartype}, Color: {car.color}</h2>
+                <button onClick={changecarproperties} >car properties</button>
+            </div> 
+            {/* eg:3 */}
+             <div>
+            <h1>I like this car: {names}</h1>
+                {
+                    names.map((name,index)=>(
+                        <h2 key={index}> {name} </h2>
+                    ))
+                }
+              <button onClick={addName}>addname</button>
+              <button onClick={deleteName}>deletename</button>
+              <div>
+      <p style={{ color: color }}>The current color is {color}</p>
+      <button onClick={colorchange}>Change Color</button>
     </div>
-  );
+                
+            </div> 
+
+
+
+        </div>
+    )
 }
 
-export default App;
+export default App
